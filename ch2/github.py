26cce2d66d5c1416a68e9ba0c8b1030ff9a057d1
Robert Meineke <rmeineke@gmail.com>
2017-04-25 03:59:22 -0700

    catching up

--- ch2/github.py
+++ ch2/github.py
@@ -2,6 +2,30 @@ import requests
 
 
 def main():
-    pass
+
+    user, repo = get_repo_info()
+    url = 'https://api.github.com/repos/{}/{}'.format(
+        user, repo
+    )
+
+    resp = requests.get(url)
+
+    if resp.status_code != 200:
+        print('Error accessing repo: {}'.format(resp.status_code))
+        return
+
+    repo_data = resp.json()
+    clone = repo_data.get('clone_url', 'ERROR: No Data')
+    print("To clone {}'s repo named {}".format(user, repo))
+    print("Git clone: {}".format(clone))
 
 
+def get_repo_info():
+    user = input("What is the username? ")
+    repo = input("Which repo? ")
+
+    return user, repo
+
+
+if __name__ == '__main__':
+    main()
